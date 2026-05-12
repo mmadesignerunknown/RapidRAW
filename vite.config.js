e26d9b888dd47d17e4d902e1b474f93c175a2201
RayJW <29835902+RayJW@users.noreply.github.com>
2026-05-12 17:09:20 +0200

    feat(build): use recommended Vite config for Tauri

--- vite.config.js
+++ vite.config.js
@@ -24,4 +24,14 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 }));
